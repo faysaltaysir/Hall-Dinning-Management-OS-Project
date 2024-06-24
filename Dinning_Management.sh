a2f4c4f7d3f5f976776549aff1a0ffdbae8f71bb
faysaltaysir <faysaltaysir1435@gmail.com>
2024-06-24 18:22:34 +0600

    admin login, meal update, file update

--- Dinning_Management.sh
+++ Dinning_Management.sh
@@ -2,16 +2,17 @@
 declare -a foods
 a=1
 USERLIST_FILE="userList.txt"
+ADMINLIST_FILE="adminList.txt"
 check_credentials() {
     local username=$1
     local password=$2
-    # local USERLIST_FILE=$3
+    local LIST_FILE=$3
     while IFS=: read -r stored_username stored_password; do
         if [[ "$username" == "$stored_username" && "$password" == "$stored_password" ]]; then
             echo "successful"
             return 0
         fi
-    done < "$USERLIST_FILE"
+    done < "$LIST_FILE"
 
     echo "failed"
     return 1
@@ -25,8 +26,8 @@ while [ $a = 1 ]
         echo "choose your option"
         echo "1) Admin Login ~> update food menu"
         echo "2) User Login ~> see food menu"
-        echo "4) User Register"
         echo "3) Exit"
+        echo "4) User Register"
         read -p "Input your choice: " temp
         echo
         echo
@@ -38,12 +39,12 @@ while [ $a = 1 ]
                 echo "## Admin Login Interface ##"
                 echo "Input your credential"
                 
-                read -p "Username: " user 
-                read -p "Password: " pass
-                res=$(check_credentials "$user" "$pass")
-                echo "Login $res"
+                read -p "Username: " userX 
+                read -p "Password: " passX
+                res2=$(check_credentials "$userX" "$passX" "$ADMINLIST_FILE")
+                echo "Login $res2"
                 
-                if [ $res = "successful" ];then
+                if [ $res2 = "successful" ];then
 
                     echo "---------------------------------"
                     echo "        Successfully LogIn       "
@@ -120,12 +121,13 @@ while [ $a = 1 ]
                                 temp=-1
                             fi
                         done
-                elif [ $res != "successful" ];then
+                elif [ $res2 != "successful" ];then
 
-                    echo "press "1" to continue"
-                    echo "press "2" to Exit"
-                    read -p "Input" inp
-                    if [ inp = 2 ];then
+                    echo "1) press "1" to continue"
+                    echo "2) press "2" to Exit"
+                    read -p "Input: " inp
+                    if [ $inp = 2 ];then
+                        temp=-1;
                         break;
                     fi    
                 fi
@@ -135,19 +137,20 @@ while [ $a = 1 ]
             
         while [ $temp = 2 ]
             do
-                echo "---------------------------------"
-                echo "        Successfully LogIn       "
-                echo "---------------------------------"
+                
                 echo "## User Login Interface ##"
                 echo "Welcome $user"
                 echo 
                 echo
                 read -p "username: " userU
                 read -p "password: " passU
-                res=$(check_credentials "$userU" "$passU" "userList.txt")
+                res=$(check_credentials "$userU" "$passU" "$USERLIST_FILE")
                 echo "Login $res"
                 while [ $res = "successful" ]
                 do
+                    echo "---------------------------------"
+                    echo "        Successfully LogIn       "
+                    echo "---------------------------------"
                     echo "choose your option"
                     echo "1) See Food Menu"
                     echo "3) Logout from your profile"
@@ -183,6 +186,7 @@ while [ $a = 1 ]
                     echo "Password Matched"
                     echo "Account Created Successfully"
                     echo $username:$password >> userList.txt
+                    # echo $username:$password >> adminList.txt
                     break;
                 fi
             done
